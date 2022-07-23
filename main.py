01dcea7db8702db9932ba517217b57bf57ae9bae
BTV123 <102411013+BTV123@users.noreply.github.com>
2022-07-23 16:11:50 +0200

    Add files via upload
    https://github.com/BTV123/assignmentarguments/upload/main

---
+++ main.py
@@ -0,0 +1,41 @@
+# Do not modify these lines
+__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
+__human_name__ = 'arguments'
+
+# Add your code after this line
+from unicodedata import name
+
+
+def greet(name, greeting="Hello, <name>!"):
+    greet = name + greeting
+    return greet
+def force(mass,body):
+    force = mass * body
+    mass[float]
+    round(body,1)
+    body[str]
+    body = gravity
+    return force
+gravity = dict(
+        sun =274,
+	    jupiter = 24.92,
+        neptune =	11.15,
+        saturn =	10.44,
+        earth =	9.798,
+        uranus =	8.87,
+        venus =	8.87,
+        mars =	3.71,
+        mercury =	3.7,
+        moon =	1.62,
+        pluto =	0.58,
+)
+dict.setdefault(earth = 9.8)
+
+def pull(m1,m2 d):
+    m1[float]
+    m2[float]
+    d[float]
+    G = 6.674*10**-11
+    force = G*(m1*m2)/d**2
+    return force        
+
