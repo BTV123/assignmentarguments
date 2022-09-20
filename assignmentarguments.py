0bdb5d5701122758e5f3e1eaa5ee6dbd47681fa1
BTV123 <102411013+BTV123@users.noreply.github.com>
2022-09-20 10:20:43 +0200

    Add files via upload
    Hier een upload van de assignment arguments

--- assignmentarguments.py
+++ assignmentarguments.py
@@ -3,9 +3,12 @@ __winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
 __human_name__ = 'arguments'
 
 # Add your code after this line
-from this import d
 from unicodedata import name
 
+
+def greet(name, greeting="Hello, <name>!"):
+    greet = name + greeting
+    return greet
 def force(mass,body):
     force = mass * body
     mass[float]
@@ -35,16 +38,4 @@ def pull(m1,m2 d):
     G = 6.674*10**-11
     force = G*(m1*m2)/d**2
     return force        
-
-
-
-
-
-
-
-
-
-       
-
-
 
